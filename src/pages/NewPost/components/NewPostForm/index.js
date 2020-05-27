8868e6857566c1b7967e5f256a3bac2ec0f0fbb9
oariza <arizaomar@gmail.com>
2020-05-26 20:05:46 -0500

    ADD new post component

---
+++ src/pages/NewPost/components/NewPostForm/index.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom'
+import { Form } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+
+
+//import "./NewPostForm.css"
+
+export default class NewPostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      imageURL: '',
+      description:'',
+      author:'',
+      contentPost:'',
+      category:'',
+      estimatedReadTime:'',
+
+    };
+    this.addPost = this.addPost.bind(this)
+  }
+
+//  handleInput({ target }) {
+//    this.setState({
+//      [name]: value,
+//    });
+//  }
+
+  onChange (event) {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  addPost(event) {
+    event.preventDefault()
+    const { title, imageURL, description, author, contentPost, category, estimatedReadTime } = this.state
+    const post = {
+      title,
+      imageURL,
+      description,
+      author,
+      contentPost,
+      category,
+      estimatedReadTime
+    }
+    this.props.onSubmit(post)
+    this.setState({
+      title: '',
+      imageURL: '',
+      description:'',
+      author:'',
+      contentPost:'',
+      category:'',
+      estimatedReadTime:''
+    })
+  }
+  
+  render() {    
+    return (
+      <div className='container'>
+        <div className="container-form">
+          <Form onSubmit={this.addPost.bind(this)} >
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Title:</Form.Label>
+              <Form.Control type="string" placeholder="Title" name="title" value={this.state.title} onChange={this.onChange.bind(this)} />
+              <Form.Label>Description:</Form.Label>
+              <Form.Control type="string" placeholder="Abstrac Post" name="description" value={this.state.description} onChange={this.onChange.bind(this)} />
+              <Form.Label>Author:</Form.Label>
+              <Form.Control type="string" placeholder="Author" name="author" value={this.state.author} onChange={this.onChange.bind(this)}/>
+              <Form.Label>Image URL:</Form.Label>
+              <Form.Control type="url" placeholder="Image URL" name="imageURL" value={this.state.imageURL} onChange={this.onChange.bind(this)}/>
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>Category</Form.Label>
+              <Form.Control as="select" name="category" value={this.state.category} onChange={this.onChange.bind(this)}>
+                <option value='PROGRAMMING'>PROGRAMMING</option>
+                <option value='CSS BASICS'>CSS BASICS</option>
+                <option value='VISUAL DESING'>VISUAL DESING</option>
+                <option value='BASED ON YOUR READING HISTORY'>BASED ON YOUR READING HISTORY</option>
+            </Form.Control>
+            <Form.Group>
+            <Form.Label>Time estimated to Read:</Form.Label>
+              <Form.Control type='string' placeholder="Time estimated to Read" name="estimatedReadTime" value={this.state.estimatedReadTime} onChange={this.onChange.bind(this)}/>
+            </Form.Group>
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Container Post</Form.Label>
+              <Form.Control as="textarea" name="contentPost" value={this.state.contentPost} onChange={this.onChange.bind(this)}/>
+            </Form.Group>
+              <Button variant="dark" type="submit">
+                Submit
+              </Button>
+          </Form>          
+        </div>
+      </div>
+    )
+  }
+}
--- src/pages/NewPost/index.js
+++ src/pages/NewPost/index.js
@@ -1,16 +1,54 @@
 import React, {Component} from  'react'
-import { Form } from 'react-bootstrap'
-import { Button } from 'react-bootstrap'
 
 import Header from '../../components/Header'
 
-export default class Hola extends Component {
-  render() {
-    return (
-      <div className='container'>
-        <Header />
-        Hola desde newPost
-        </div>
+import NewPostForm from './components/NewPostForm'
+
+export default class NewPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: {},
+    };
+    //this.handlerNewPostForm = this.handlerNewPostForm.bind(this);
+  }
+
+  async onSubmit(post) {
+    console.log(post)
+    await fetch(
+      'http://blog-medium-api.mybluemix.net/posts',
+      {
+        method: 'POST',
+        body: JSON.stringify(post),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }
     )
+    .then(response => response.json())
+    .then(({name}) => {
+      console.log('Estamos aquí')
+      const { posts } = this.state
+      post['key'] = name
+      this.setState({
+        posts: {...posts, post}
+      })
+    })
+  }
+
+  render() {
+    console.log('estamos en el render newpost')
+    return (
+      <div className="Container">
+        <div>
+          <Header />
+          <div className="Post-container">
+            <NewPostForm 
+              onSubmit={this.onSubmit.bind(this)}
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
